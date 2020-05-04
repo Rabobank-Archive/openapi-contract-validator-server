7ab5c2d0f2d4a02a82654c14d34bd103bac02d5c
Sander van Beek <sander.van.beek@rabobank.nl>
2020-05-04 19:09:14 +0200

    Removed `apiaryio`. Replaced implementation with `got`

--- src/endpoint/contract-downloader/apiary/fetch.js
+++ src/endpoint/contract-downloader/apiary/fetch.js
@@ -1,9 +1,8 @@
-const apiary = require('apiaryio');
 const _ = require('lodash');
+const got = require('got');
 const yaml = require('js-yaml');
 
 const apiaryToken = require('./token');
-const logger = require('../../../logger');
 
 /**
  * Fetch a contract from Apiary
@@ -14,56 +13,60 @@ const logger = require('../../../logger');
  * @param {serverConfig.apiary} config
  * @return {Promise.<object>}
  */
-function fetchFromApiary(apiaryId, config) {
-    return new Promise((resolve, reject) => {
-        apiary.fetch(apiaryId, apiaryToken.get(config),
-            (response) => {
-                if (!_.isString(response)) {
-                    reject(new TypeError('API response is not a string'));
-                    return;
-                }
-                if (_.isEmpty(response)) {
-                    reject(new TypeError('API response is empty'));
-                    return;
-                }
+async function fetchFromApiary(apiaryId, config) {
+    const result = await got.get(`https://api.apiary.io/blueprint/get/${apiaryId}`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'text/plain',
+            'Authentication': `Token ${apiaryToken.get(config)}`,
+        },
+        responseType: 'json',
+    })
+        .catch(errorDecorator);
 
-                try {
-                    const parsed = yaml.safeLoad(response);
-                    if (_.isString(parsed)) {
-                        throw new TypeError('Response could not be parsed as JSON or YAML');
-                    }
+    const response = result.body;
+    if (response.error) {
+        throw new Error(`Apiary responded with an error: ${response.message}`);
+    }
 
-                    resolve(parsed);
-                }
-                catch (error) {
-                    logger.log(0, `Could not fetch valid contract from Apiary: `
-                        + error.message);
-                    resolve(response);
-                }
-            },
-            (errorResponse) => {
-                try {
-                    const error = new Error('Could not fetch valid contract from Apiary');
+    const contract = response.code;
+    if (!_.isString(contract)) {
+        throw new TypeError('Contract in API response is not a string');
+    }
+    if (_.isEmpty(contract)) {
+        throw new TypeError('Contract in API response is empty');
+    }
 
-                    const body = JSON.parse(errorResponse.body);
-                    if (_.isEmpty(body.message)) {
-                        throw error;
-                    }
-
-                    error.message += `: ${body.message}`;
-                    reject(error);
-                }
-                catch (error) {
-                    reject(new Error(
-                        'Could not fetch valid contract from Apiary for an unkown reason\n'
-                        + 'Possible reasons for this failure:\n'
-                        + '1. You have not provided a (valid) Apiary token\n'
-                        + '   Note: You can generate a token @ https://login.apiary.io/tokens\n'
-                        + '2. The token provided does not have read permissions\n'
-                        + '3. You can\'t reach Apiary'));
-                }
-            },
-        );
-    });
+    const parsed = yaml.safeLoad(contract);
+    if (_.isString(parsed)) {
+        throw new TypeError('Contract in API response could not be parsed as JSON or YAML');
+    }
+    return parsed;
 };
 module.exports = _.memoize(fetchFromApiary);
+
+
+/**
+ * @param {HTTPError} apiError
+ */
+function errorDecorator(apiError) {
+    const error = new Error('Could not fetch valid contract from Apiary');
+    error.message += `: ${apiError.response.statusCode}`;
+
+    const body = apiError.response.body;
+    if (_.isEmpty(body.message)) {
+        throw error;
+    }
+
+    error.message += `: ${body.message}`;
+
+    if (apiError.response.statusCode === 403) {
+        error.message += '\n'
+            + 'Probable reasons for this failure:\n'
+            + '1. You have not provided a (valid) Apiary token\n'
+            + '   Note: You can generate a token @ https://login.apiary.io/tokens\n'
+            + '2. The token provided does not have read permissions\n'
+            + '3. Your account is not allowed to access the contract';
+    }
+    throw error;
+}
